9cef7ca95a4ab9a4e62b433b79a997c05724a5ea
NassimBous <nassimbouslimani2005@gmail.com>
2026-06-13 09:35:23 +0200

    Ajout de la javaDoc de Site.

--- src/main/java/pgl/app/model/Site.java
+++ src/main/java/pgl/app/model/Site.java
@@ -6,7 +6,10 @@ package pgl.app.model;
  * @version 1.0
  */
 public class Site extends Point{
-	
+
+	/**
+	 * The unique identifier for this site.
+	 */
 	private int id;
 
 	/** Creates a new site with an identifier and coordinates.
@@ -19,18 +22,34 @@ public class Site extends Point{
 		super(x, y);
 		this.setId(id);
 	}
-	
+
+	/**
+	 * Retrieves the unique identifier of the site.
+	 *
+	 * @return The integer ID of the site.
+	 */
 	public int getId() {
 		return id;
 	}
-	
+
+	/**
+	 * Sets the unique identifier for the site.
+	 *
+	 * @param id The new integer ID to assign to the site.
+	 * @throws IllegalArgumentException If the provided ID is negative.
+	 */
 	public void setId(int id) {
 		if(id < 0) {
 			throw new IllegalArgumentException("L'id du site ne peut pas être négatif");
 		}
 		this.id = id;
 	}
-	
+
+	/**
+	 * Returns a string representation of the site, including its ID and spatial coordinates.
+	 *
+	 * @return A formatted string describing the site.
+	 */
 	@Override
 	public String toString() {
 		return "Site {" + "id =" + getId() + super.toString() + "}";
